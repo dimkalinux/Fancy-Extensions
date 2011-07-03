63ddbac515faa5865c5f6031d8d5461b99bf0b40
dimkalinux <dimka.linux@gmail.com>
2011-07-03 12:03:12 +0300

    Add fancy_merge_posts to repo

---
+++ fancy_merge_posts/lang/Russian/fancy_merge_posts.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$fancy_merge_posts = array(
+	'Name' =>	'Fancy Merge Posts параметры',
+	'Dont Merge'	=>	'Не склеивать сообщение',
+	'Allow dont merge' => 'Разрешить не склеивать сообщения.',
+	'Added'	=>	'Добавлено: %s',
+	'Silent mode desc' => 'Склеивать сообщения в «тихом» режиме (без «Добавлено»)',
+	'No update date desc' => 'Не обновлять дату сообщения',
+	'Dont merge timeout' => 'Не склеивать после',
+	'Dont merge timeout hint' => 'В минутах. Если 0 — склеивать всегда',
+	'Go to settings'		=> 'Настройки',
+);
+
+?>
