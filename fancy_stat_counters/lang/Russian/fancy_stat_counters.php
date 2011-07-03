3be3cd0470950b21d7ac93fc85bd2e931ced8a87
dimkalinux <dimka.linux@gmail.com>
2011-07-03 11:38:44 +0300

    Add fancy_stat_counters to repo

---
+++ fancy_stat_counters/lang/Russian/fancy_stat_counters.php
@@ -0,0 +1,19 @@
+<?php
+
+// Language definitions used in pun_pm
+$lang_fancy_stat_counters = array(
+	'Settings Name GA'		=> 'Fancy Stat Counters: Google Analytics',
+	'Enabled'				=> 'Включено',
+	'ID'					=> 'ID счётчика',
+
+	'GA'					=> 'Google Analytics',
+	'GA ID Name'			=> 'ID счётчика',
+	'GA ID Help'			=> 'Например: UA-00000-01',
+
+	'Settings Name YM'		=> 'Fancy Stat Counters: Яндекс Метрика',
+	'YM'					=> 'Яндекс Метрика',
+	'YM ID Name'			=> 'ID счётчика',
+	'YM ID Help'			=> 'Например: 7500000',
+);
+
+?>
