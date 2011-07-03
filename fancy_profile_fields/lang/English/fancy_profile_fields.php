2012ea7c6d407990703f34023a996fa612bb0191
dimkalinux <dimka.linux@gmail.com>
2011-07-03 11:51:55 +0300

    Add fancy_profile_fields to repo

---
+++ fancy_profile_fields/lang/English/fancy_profile_fields.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$fancy_profile_fields = array(
+	'Skype'		=>	'Skype',
+	'Vkontakte'	=>	'Vkontakte',
+	'Twitter'	=>	'Twitter',
+	'Facebook'	=>	'Facebook',
+	'Phone'		=>	'Phone',
+	'Linkedin'	=>	'LinkedIn',
+);
+
+?>
---
+++ fancy_profile_fields/lang/Russian/fancy_profile_fields.php
@@ -0,0 +1,14 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$fancy_profile_fields = array(
+	'Skype'	=>	'Скайп',
+	'Vkontakte'	=>	'ВКонтакте',
+	'Twitter'	=>	'Твитер',
+	'Facebook'	=>	'Фейсбук',
+	'Phone'		=>	'Телефон',
+	'Linkedin'	=>	'LinkedIn',
+);
+
+?>
