59dec6e743bc41eae7a2d30a07b6d281f72bc026
dimkalinux <dimka.linux@gmail.com>
2011-07-03 11:34:01 +0300

    Add fancy_video_tag to repo

---
+++ fancy_video_tag/lang/English/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+	$fancy_video_tag = array(
+		'Name' 					=> 'Settings for Fancy Video Tag',
+		'unknown_source'		=> 'video',
+		'no flash'				=> 'video (flash player not installed)',
+		'Go to settings'		=> 'Settings',
+		'HTML5 mode desc' 		=> 'Use HTML5 (without "Flash")',
+		'video_uri'				=> 'http://my.video.com/xyz',
+		'video_display'			=> 'an embedded video player in your post.<br />You can use any video uri from the following services: youtube, dailymotion, vimeo, google video.',
+	);
+?>
---
+++ fancy_video_tag/lang/Russian/lang.php
@@ -0,0 +1,12 @@
+<?php
+
+	$fancy_video_tag = array(
+		'Name' 					=>	'Fancy Video Tag параметры',
+		'unknown_source'		=> 'видео',
+		'no flash'				=> 'видео (не установлен флеш-плагин)',
+		'Go to settings'		=> 'Настройки',
+		'HTML5 mode desc' 		=> 'Использовать HTML5 (без "Адоуб Флеш")',
+		'video_uri'				=>	'http://my.video.com/xyz',
+		'video_display'			=>	'встроенный видеоплеер в вашем сообщении.<br />Вы можете использовать видео только с этих сервисов: youtube, rutube, dailymotion, vimeo, google video.',
+	);
+?>
