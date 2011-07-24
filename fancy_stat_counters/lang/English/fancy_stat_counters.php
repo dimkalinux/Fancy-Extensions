02cb5705168f7b9f38a2de1f571c4420bd8f9455
dimkalinux <dimka.linux@gmail.com>
2011-07-24 22:21:28 +0300

    fancy_stat_counters: add to repoa and update to 1.4

---
+++ fancy_stat_counters/lang/English/fancy_stat_counters.php
@@ -0,0 +1,21 @@
+<?php
+
+// Language definitions used in pun_pm
+$lang_fancy_stat_counters = array(
+	'Settings Name GA'		=> 'Fancy Stat Counters: Google Analytics',
+	'Enabled'				=> 'Enabled',
+	'ID'					=> 'Counter ID',
+
+	'GA'					=> 'Google Analytics',
+	'GA ID Name'			=> 'Counter ID',
+	'GA ID Help'			=> 'For example: UA-00000-01',
+
+	'Settings Name YM'		=> 'Fancy Stat Counters: Yandex Metrics',
+	'YM'					=> 'Yandex Metrics',
+	'YM ID Name'			=> 'Counter ID',
+	'YM ID Help'			=> 'For example: 7500000',
+
+	'Settings'				=> 'Settings',
+);
+
+?>
---
+++ fancy_stat_counters/lang/Russian/fancy_stat_counters.php
@@ -0,0 +1,21 @@
+<?php
+
+// Language definitions used in pun_pm
+$lang_fancy_stat_counters = array(
+	'Settings Name GA'		=> 'Fancy Stat Counters: Google Analytics',
+	'Enabled'				=> 'Включен',
+	'ID'					=> 'ID счётчика',
+
+	'GA'					=> 'Google Analytics',
+	'GA ID Name'			=> 'ID счётчика',
+	'GA ID Help'			=> 'Например: UA-00000-01',
+
+	'Settings Name YM'		=> 'Fancy Stat Counters: Яндекс Метрика',
+	'YM'					=> 'Яндекс Метрика',
+	'YM ID Name'			=> 'ID счётчика',
+	'YM ID Help'			=> 'Например: 7500000',
+
+	'Settings'				=> 'Настройки',
+);
+
+?>
