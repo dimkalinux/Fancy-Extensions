89f51b7c582a52f29cd5f0425937fa7119fc6851
dimkalinux <dimka.linux@gmail.com>
2011-07-26 14:30:18 +0300

    last_topic_title_on_forum_index: add to repo and update to 1.4

---
+++ last_topic_title_on_forum_index/lang/Russian/last_topic_title_on_forum_index.php
@@ -0,0 +1,15 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_last_topic_title_on_forum_index = array(
+	'Input' 	=>	'Длина топиков на главной странице',
+	'Select Mode' => 'Стиль топиков на главной странице',
+	'Mode_1' 	=> 'В две строки',
+	'Mode_2' 	=> 'В три строки',
+	'Name'		=> 'Last Topic Title параметры',
+	'Go to settings'		=> 'Настройки',
+	'Sync all forums'		=> 'Синхронизировать все форумы',
+);
+
+?>
