b2a027359bbf8f2108391ffe039617b4fa31ad20
dimkalinux <dimka.linux@gmail.com>
2011-08-14 16:14:22 +0300

    fancy_spoiler: fix error in Russian lang

--- fancy_spoiler/lang/Russian/lang.php
+++ fancy_spoiler/lang/Russian/lang.php
@@ -2,7 +2,7 @@
 
 if (!defined('FORUM')) die();
 
-$lang_spoiler_bbcode = array(
+$lang_fancy_spoiler = array(
 	'spoiler_open'	=>	'продолжить читать',
 	'spoiler_close'	=>	'скрыть спойлер',
 	'spoiler_example'	=>	'Long long text'
