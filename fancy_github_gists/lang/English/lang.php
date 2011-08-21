bb244c298b111c943db84fea68e552ddaedf276f
dimkalinux <dimka.linux@gmail.com>
2011-08-21 09:55:38 +0300

    fancy_github_gists: add to repo

---
+++ fancy_github_gists/lang/English/lang.php
@@ -0,0 +1,7 @@
+<?php
+
+	$fancy_github_gists_lang = array(
+		'gist_uri'			=> 'https://gist.github.com/1094560',
+		'gist_display'		=> 'an embedded gist block in your post.',
+	);
+?>
---
+++ fancy_github_gists/lang/Russian/lang.php
@@ -0,0 +1,7 @@
+<?php
+
+	$fancy_github_gists_lang = array(
+		'gist_uri'			=> 'https://gist.github.com/1094560',
+		'gist_display'		=> 'встроенный gist блок в сообщении.',
+	);
+?>
