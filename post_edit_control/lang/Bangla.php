0f3636a3a69d366c5136a136818c9edc9dc6f2cb
dimkalinux <dimka.linux@gmail.com>
2011-08-22 18:32:04 +0300

    post_edit_control: add to repo

---
+++ post_edit_control/lang/Bangla.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_post_edit_control = array(
+	'Edit period'		=> 'Post Edit Control',
+	'Duration'			=> 'Duration',
+	'Duration note'		=> 'Number of Minutes  a post can be edited after posting. You can enter days by converting them into minutes.',
+	'No edit'			=> 'দুঃখিত, আপনি এই পোস্ট সম্পাদনা করতে পারবেন না কারণ সম্পাদনা করার সময়সীমা পার হয়ে গেছে। আপনি যদি মনে করে এই পোস্টে কোন সংশোধনী প্রয়োজন তাহলে আপনি পোস্টের নিচের অভিযোগ লিংকটি ব্যবহার করে মডারেশন বোর্ডকে তা অবহিত করতে পারেন।',
+
+	'Block edit'		=> 'Block editing after replies',
+	'Block edit desc'	=> 'If you check this, users will not be able to edit post unless it is last post of topic regardless of editing period of time.',
+
+	'Last reply edit'		=> 'Block replying after own last reply',
+	'Last reply edit desc'	=> 'Enabling this will force user to edit his/her post rather making more replies until someone else is replied.',
+	'Last reply alert'		=> 'দুঃখিত, অন্য কেউ উত্তর দেয়ার পূর্ব পর্যন্ত আপনি আর নতুন উত্তর দিতে পারবেন না। সংযোজন, বিয়োজন বা সংশোধন এর জন্য আপনি আপনার সর্বশেষ উত্তরটি (পোস্টটি) সম্পাদনা করুন। আপনি যদি সর্বশেষ পোস্টটি সম্পাদনা করতে না পারেন আবার নতুন উত্তরও দিতে না পারেন সেক্ষেত্রে অনুগ্রহ করে বিষয়টি কর্তৃপক্ষের দৃষ্টিগোচর করুন।',
+);
---
+++ post_edit_control/lang/English.php
@@ -0,0 +1,17 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_post_edit_control = array(
+	'Edit period'		=> 'Post Edit Control',
+	'Duration'			=> 'Duration',
+	'Duration note'		=> 'Number of Minutes  a post can be edited after posting. You can enter days by converting them into minutes.',
+	'No edit'			=> 'You can\'t edit this post anymore as either editing time is expired or someone else replied after your post. If you need to correct anything please report to Admin and Moderators. They will take care of that.',
+
+	'Block edit'		=> 'Block editing after replies',
+	'Block edit desc'	=> 'If you check this, users will not be able to edit post unless it is last post of topic regardless of editing period of time.',
+
+	'Last reply edit'		=> 'Block replying after own last reply',
+	'Last reply edit desc'	=> 'Enabling this will force user to edit his/her post rather making more replies until someone else is replied.',
+	'Last reply alert'		=> 'Sorry! you can\'t reply this topic until someone else replies. Please edit your last post rather than making a new reply',
+);
