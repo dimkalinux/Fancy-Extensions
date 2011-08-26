7f80288b8657ad47c495baccdc88693e1c7a0c7b
dimkalinux <dimka.linux@gmail.com>
2011-08-26 20:37:47 +0300

    fancy_forum_archive: add to repo

---
+++ fancy_forum_archive/lang/English/fancy_forum_archive.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_fancy_forum_archive = array(
+	'Archive'	=> 'forum in archive',
+	'MainMenu'	=> 'Archive',
+);
+
+?>
---
+++ fancy_forum_archive/lang/Russian/fancy_forum_archive.php
@@ -0,0 +1,10 @@
+<?php
+
+if (!defined('FORUM')) die();
+
+$lang_fancy_forum_archive = array(
+	'Archive'	=> 'форум в архиве',
+	'MainMenu'	=> 'Архив',
+);
+
+?>
