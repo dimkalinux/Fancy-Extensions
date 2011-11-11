66d6da63cb74c4754442cba24256289e58acf30d
dimkalinux <dimka.linux@gmail.com>
2011-11-11 23:51:12 +0200

    Update fancy_scroll_to_top: new easing algo

--- fancy_scroll_to_top/js/fancy_scroll_to_top.js
+++ fancy_scroll_to_top/js/fancy_scroll_to_top.js
@@ -2,13 +2,13 @@
 //** Available/ usage terms at http://www.dynamicdrive.com (March 30th, 09')
 //** v1.1 (April 7th, 09'):
 //** 1) Adds ability to scroll to an absolute position (from top of page) or specific element on the page instead.
-//** 2) Fixes scroll animation not working in Opera. 
+//** 2) Fixes scroll animation not working in Opera.
 
 
 var scrolltotop = {
     //startline: Integer. Number of pixels from top of doc scrollbar is scrolled before showing control
     //scrollto: Keyword (Integer, or "Scroll_to_Element_ID"). How far to scroll document up when control is clicked on (0=top).
-    setting: {startline:PUNBB.env.fancy_scroll_to_top.startline, scrollto: 0, scrollduration:30, fadeduration:[500, 200]},
+    setting: {startline:PUNBB.env.fancy_scroll_to_top.startline, scrollto: 0, scrollduration: 9, fadeduration:[500, 200]},
     controlHTML: '&uarr;', //HTML for control, which is auto wrapped in DIV w/ ID="topcontrol"
     controlattrs: {offsetx:5, offsety:5}, //offset of control relative to right/ bottom of window corner
     anchorkeyword: '#top', //Enter href value of HTML anchors on the page that should also act as "Scroll Up" links
@@ -18,7 +18,7 @@ var scrolltotop = {
     scrollup: function () {
         if (!this.cssfixedsupport) //if control is positioned using JavaScript
             this.$control.css({opacity:0}) //hide control immediately after clicking it
-        
+
         var dest=isNaN(this.setting.scrollto)? this.setting.scrollto : parseInt(this.setting.scrollto)
         if (typeof dest=="string" && jQuery('#'+dest).length==1) //check element set by string exists
             dest=jQuery('#'+dest).offset().top;
@@ -26,12 +26,17 @@ var scrolltotop = {
             dest=0;
 
         var r = parseInt(((jQuery(window).scrollTop() - dest) / 100), 10);
-        if (!r || r < 15) {
+        if (!r || r < 10) {
+            r = 10;
+        } else if (!r || r > 15) {
             r = 15;
-        } else if (!r || r > 50) {
-            r = 50;
         }
-        this.$body.animate({scrollTop: dest}, r * this.setting.scrollduration);
+
+        jQuery.easing['fancy_scroll_top'] = function (x, t, b, c, d) {
+            return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+        };
+
+        this.$body.animate({scrollTop: dest}, r * this.setting.scrollduration, 'fancy_scroll_top');
     },
 
     keepfixed: function () {
@@ -48,7 +53,7 @@ var scrolltotop = {
         if (!this.cssfixedsupport) {
             this.keepfixed();
         }
-        
+
         this.state.shouldvisible=(scrolltop>=this.setting.startline)? true : false
         if (this.state.shouldvisible && !this.state.isvisible) {
             this.$control.stop().animate({opacity:1}, this.setting.fadeduration[0]);
@@ -58,7 +63,7 @@ var scrolltotop = {
             this.state.isvisible = false;
         }
     },
-    
+
     init:function(){
         jQuery(document).ready(function($){
             var mainobj=scrolltotop
@@ -73,7 +78,7 @@ var scrolltotop = {
             if (document.all && !window.XMLHttpRequest && mainobj.$control.text()!='') //loose check for IE6 and below, plus whether control contains any text
                 mainobj.$control.css({width:mainobj.$control.width()}) //IE6- seems to require an explicit width on a DIV containing text
             mainobj.togglecontrol()
-            
+
             $(window).bind('scroll resize', function(e){
                 mainobj.togglecontrol();
             })
