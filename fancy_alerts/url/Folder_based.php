f2b9c0b5851778e361d39c2a04eab1f86d003bfa
anatoliyz <anatoliyz@localhost>
2019-04-16 13:57:44 +0300

    - seo-friendly urls in alerts - paging in alerts

--- fancy_alerts/url/Folder_based.php
+++ fancy_alerts/url/Folder_based.php
@@ -5,15 +5,15 @@ if (!defined('FORUM')) {
 	exit;
 }
 
-$forum_url['fancy_alerts_topics_goto_alerts'] 		= 'search.php?action=fancy_alerts_topics_show&amp;user_id=$1';
-$forum_url['fancy_alerts_quotes_goto_alerts'] 		= 'search.php?action=fancy_alerts_quotes_show&amp;user_id=$1';
+$forum_url['fancy_alerts_topics_goto_alerts'] 		= 'search/alerts/topics/$1';
+$forum_url['fancy_alerts_quotes_goto_alerts'] 		= 'search/alerts/quotes/$1';
 $forum_url['fancy_alerts_quotes_mark_read'] 		= 'misc.php?action=fancy_alerts_quotes_mark_read&amp;csrf_token=$1';
 $forum_url['fancy_alerts_topics_mark_read'] 		= 'misc.php?action=fancy_alerts_topics_mark_read&amp;csrf_token=$1';
 
 $forum_url['fancy_alerts_topics_turn_on'] 			= 'misc.php?action=fancy_alerts_topics_on&amp;tid=$1&amp;csrf_token=$2';
 $forum_url['fancy_alerts_topics_turn_off'] 			= 'misc.php?action=fancy_alerts_topics_off&amp;tid=$1&amp;csrf_token=$2';
 
-$forum_url['fancy_alerts_search_my_alerts_topics'] 	= 'search.php?action=show_fancy_alerts_my_topics&amp;user_id=$1';
+$forum_url['fancy_alerts_search_my_alerts_topics'] 	= 'search/alerts/my/topics/$1';
 
 $forum_url['fancy_alerts_autoupdate_status'] 		= 'misc.php?action=fancy_alerts_update_status';
 
